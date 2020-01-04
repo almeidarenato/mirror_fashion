94354d95fb5c073710c7a2a1a29658885e1fd664
Mateus V. Farias <55674918+fariasmateuss@users.noreply.github.com>
2020-01-03 23:27:42 -0400

    File Js has been added to change banner and adjust in css status bar

--- js/produto.js
+++ js/produto.js
@@ -2,7 +2,7 @@ var inputTamanho = document.querySelector('[name=tamanho]');
 var outputTamanho = document.querySelector('[name=valortamanho]');
 
 function mostraTamanho(){
-    outputTamanho.value = inputTamanho.value
+    outputTamanho.value = inputTamanho.value;
 }
 
-inputTamanho.oninput = mostraTamanho
+inputTamanho.oninput = mostraTamanho;
