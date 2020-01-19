aa0122a9584bbcfff7613db40fc106813f38e525
Mateus V. Farias <55674918+fariasmateuss@users.noreply.github.com>
2020-01-18 23:44:32 -0400

    addded conversion of code

--- js/produto.js
+++ js/produto.js
@@ -1,7 +1,8 @@
+
 var inputTamanho = document.querySelector('[name=tamanho]');
 var outputTamanho = document.querySelector('[name=valortamanho]');
 
-function mostraTamanho(){
+function mostraTamanho() {
     outputTamanho.value = inputTamanho.value;
 }
 
